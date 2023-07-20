78933f2adc457f280cc203f7a3333fdcc373d864
TuanNT <tuan@wearebloom.com.au>
2023-07-20 14:35:37 +0700

    INIT layout

---
+++ src/components/filter.tsx
@@ -0,0 +1,93 @@
+import { Disclosure } from "@headlessui/react"
+import { MinusIcon, PlusIcon } from "@heroicons/react/20/solid"
+import { useEffect, useState } from "react"
+import { Genre, GenreEntity } from "../helpers/genre-entity"
+
+
+
+type FilterOption = {
+  value: string
+  label: string
+  checked: boolean
+
+}
+
+type Filter = {
+  id: string
+  name: string
+  options: Array<FilterOption>
+}
+
+
+const FilterComponent = () => {
+
+  const [filters, setFilters] = useState<Array<Filter>>([])
+
+  useEffect(() => {
+
+    new GenreEntity().getGenres().then(genres => {
+
+      console.log(genres)
+      const options = genres.map((g: Genre): FilterOption => ({
+        value: String(g.id),
+        label: g.name,
+        checked: false,
+      }))
+
+      setFilters([{ id: 'genre', name: 'Genres', options }])
+    })
+
+  }, [])
+
+  return (
+
+    <div className="mt-4 border-t border-gray-200">
+      <h3 className="sr-only">Genres</h3>
+
+      {filters && filters.map((section) => (
+        <Disclosure as="div" key={section.id} className="border-t border-gray-200 px-4 py-6">
+          {({ open }) => (
+            <>
+              <h3 className="-mx-2 -my-3 flow-root">
+                <Disclosure.Button className="flex w-full items-center justify-between bg-white px-2 py-3 text-gray-400 hover:text-gray-500">
+                  <span className="font-medium text-gray-900">{section.name}</span>
+                  <span className="ml-6 flex items-center">
+                    {open ? (
+                      <MinusIcon className="h-5 w-5" aria-hidden="true" />
+                    ) : (
+                      <PlusIcon className="h-5 w-5" aria-hidden="true" />
+                    )}
+                  </span>
+                </Disclosure.Button>
+              </h3>
+              <Disclosure.Panel className="pt-6">
+                <div className="space-y-6">
+                  {section.options.map((option, optionIdx) => (
+                    <div key={option.value} className="flex items-center">
+                      <input
+                        id={`filter-mobile-${section.id}-${optionIdx}`}
+                        name={`${section.id}[]`}
+                        defaultValue={option.value}
+                        type="checkbox"
+                        defaultChecked={option.checked}
+                        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                      />
+                      <label
+                        htmlFor={`filter-mobile-${section.id}-${optionIdx}`}
+                        className="ml-3 min-w-0 flex-1 text-gray-500"
+                      >
+                        {option.label}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      ))}
+    </div>
+  )
+}
+
+export default FilterComponent
---
+++ src/helpers/genre-entity.ts
@@ -0,0 +1,29 @@
+export type Genre = {
+  id: number;
+  name: string;
+};
+
+const host = "http://localhost:8000/api";
+const token =
+  "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1ER0QyYUw4M2xabHBCcGE1Tnp5LSJ9.eyJpc3MiOiJodHRwczovL2Rldi14MDdlYm8wYzRkeG14eGdqLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiIxUmU4Q0xRMndJZ3lkU05BU3BRSk1ZSXM5bWRMdFVSZkBjbGllbnRzIiwiYXVkIjoiUVdObFJHbG5hWFJoYkE9PSIsImlhdCI6MTY4OTgzNTE0MCwiZXhwIjoxNjg5OTIxNTQwLCJhenAiOiIxUmU4Q0xRMndJZ3lkU05BU3BRSk1ZSXM5bWRMdFVSZiIsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyJ9.4d4bKmEUnvDyi4iJwCtRUdlTrwxH30WsyV5FRMB4mFJuNW3P1Aqb6p4LpDd4znzxElHrDmsgIDX7YpnsdUzg_rkMwmRdb0cCJL2Az12BFz6mY7P09GkDOqkStLg3QJtIJ-jl2ZVoZ4TFFZue88VakQe4CUZL0oj-kUT9QPHtLLmdT2wKLxWTpiMmb17H4oOAy1aX4k3cLmfZwLo6rrHVt8CKq0vgvNaTD7i2xwyQF2O29cjh_oKka5J_C35nzgRJ1JqXYbTzqK7KcqWxi0TqRZE3DXGLA-PeYLezB8Vogr60US_SpZkIPyfynXgZwE529CzYcVc7dU-jtawXDwuZZQ";
+
+export class GenreEntity {
+  private uri: string = "/genres";
+
+  public async getGenres(): Promise<Array<Genre>> {
+    try {
+      const genres: Array<Genre> = await fetch(host + this.uri, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+
+      return genres;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+}
